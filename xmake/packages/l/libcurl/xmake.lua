123f62ac7ea18a2ad09e37a20637e56de2f54da6
wumo <he17802723@163.com>
2026-03-20 15:09:26 +0800

    test

--- xmake/packages/l/libcurl/xmake.lua
+++ xmake/packages/l/libcurl/xmake.lua
@@ -1,13 +1,12 @@
 package("libcurl")
-    set_homepage("https://curl.haxx.se/")
+    set_homepage("https://curl.se/")
     set_description("The multiprotocol file transfer library.")
     set_license("MIT")
 
-    set_urls("https://curl.haxx.se/download/curl-$(version).tar.bz2")
+    set_urls("https://curl.se/download/curl-$(version).tar.bz2")
     add_urls("https://github.com/curl/curl/releases/download/curl-$(version).tar.bz2",
         {version = function (version) return (version:gsub("%.", "_")) .. "/curl-" .. version end})
-    add_urls("https://gitee.com/mirrors/curl.git")
-    add_versions("v8.11.1", "curl-8_11_1")
+    add_versions("8.11.1", "e9773ad1dfa21aedbfe8e1ef24c9478fa780b1b3d4f763c98dd04629b5e43485")
 
     add_configs("cares",    {description = "Enable c-ares support.", default = false, type = "boolean"})
     add_configs("openssl",  {description = "Enable OpenSSL for SSL/TLS.", default = nil, type = "boolean"})
