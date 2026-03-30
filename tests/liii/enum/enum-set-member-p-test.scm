62d5f3fa405b4d9f518a62215d4bb5b2d7aa55e2
wumo <he17802723@163.com>
2026-03-30 14:13:32 +0800

    wip

--- tests/liii/enum/enum-set-member-p-test.scm
+++ tests/liii/enum/enum-set-member-p-test.scm
@@ -1,7 +1,6 @@
 (import (liii check)
         (liii enum)
-        (srfi srfi-1)
-        (srfi srfi-128))
+        (srfi srfi-1))
 
 (check-set-mode! 'report-failed)
 
@@ -10,46 +9,11 @@
 
 (define color (make-enum-type color-names))
 
-(define color-red (enum-name->enum color 'red))
-(define color-tangerine (enum-name->enum color 'tangerine))
-(define color-blue (enum-name->enum color 'blue))
-(define color-green (enum-name->enum color 'green))
-(define color-set (enum-type->enum-set color))
-
 (define reddish
   (list->enum-set color
                   (map (lambda (name)
                          (enum-name->enum color name))
                        (take color-names 3))))
-
-(define reddish-complement
-  (list->enum-set color
-                  (map (lambda (name)
-                         (enum-name->enum color name))
-                       (drop color-names 3))))
-
-(define empty-colors (enum-empty-set color))
-
-(define pizza-descriptions
-  '((margherita "tomato and mozzarella")
-    (funghi "mushrooms")
-    (bianca "ricotta and mozzarella")
-    (chicago "deep-dish")
-    (hawaiian "pineapple and ham")))
-
-(define pizza-names (map car pizza-descriptions))
-(define pizza (make-enum-type pizza-descriptions))
-(define pizza-chicago (enum-name->enum pizza 'chicago))
-(define pizza-bianca (enum-name->enum pizza 'bianca))
-
-(define (constantly obj)
-  (lambda _ obj))
-
-(define always (constantly #t))
-(define never (constantly #f))
-
-(define (fresh-sets proc eset1 eset2)
-  (proc (enum-set-copy eset1) (enum-set-copy eset2)))
 
 ;; enum-set-member?
 ;; 判断符号是否是 enum-set 的成员名称。
