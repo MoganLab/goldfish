1fe7612fd0da3357e599cb30fc58ec6bdb8e9f45
Da Shen <da@liii.pro>
2026-03-30 14:47:00 +0800

    [216_16] 规范化 bang 版本测试文件的文档格式

--- tests/liii/iset/iset-difference-bang-test.scm
+++ tests/liii/iset/iset-difference-bang-test.scm
@@ -8,6 +8,22 @@
 ;; iset-difference!
 ;; 与 iset-difference 相同，但可以修改第一个集合。
 ;;
+;; 语法
+;; ----
+;; (iset-difference! iset1 iset2 ...)
+;;
+;; 参数
+;; ----
+;; iset1 : iset
+;; 要修改的集合。
+;;
+;; iset2 ... : iset
+;; 要减去的集合。
+;;
+;; 返回值
+;; -----
+;; 返回修改后的原 iset。
+;;
 (check (iset->list (iset-difference! (iset 0 1 3 4) (iset 0 2) (iset 0 4)))
        => '(1 3)
 ) ;check
