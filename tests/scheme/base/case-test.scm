1a14ab67b88d7ced989b6c2ccbadef2a3b7e159c
Darcy Shen <da@liii.pro>
2026-04-04 20:58:52 +0800

    [216_7] 更新 case-test.scm，简化依赖并完善文档 (#669)

--- tests/scheme/base/case-test.scm
+++ tests/scheme/base/case-test.scm
@@ -1,14 +1,24 @@
 (import (liii check)
-        (liii base)
-        (liii list)
-        (liii case)
-        (liii error)
-        (liii os))
+        (scheme base))
 
 (check-set-mode! 'report-failed)
 
 ;; case
-;; 测试 case 分支表达式。
+;; case 是 R7RS 定义的多分支条件选择表达式，根据 key 值匹配不同的子句执行。
+;;
+;; 说明
+;; ----
+;; case 是 R7RS 定义的分支表达式。
+;;
+;; 局限性
+;; ----
+;; case 使用 eqv? 进行匹配，只能精确比较值，不支持自定义匹配逻辑。
+;;
+;; 升级版 case*
+;; ----
+;; case* 是 case 的升级版，提供更灵活的匹配能力。
+;; 位于 (liii case) 库中，可使用以下命令查看文档：
+;;   gf doc liii/case
 ;;
 ;; 语法
 ;; ----
