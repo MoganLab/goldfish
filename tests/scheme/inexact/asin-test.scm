d8892e8ad08940e67f1af6a619444f6a86eefde6
Da Shen <da@liii.pro>
2026-04-15 14:39:43 +0800

    gf fmt tests/scheme/base

--- tests/scheme/inexact/asin-test.scm
+++ tests/scheme/inexact/asin-test.scm
@@ -1,9 +1,5 @@
-(import (liii check)
-        (scheme inexact)
-) ;import
-
+(import (liii check) (scheme inexact))
 (check-set-mode! 'report-failed)
-
 ;; asin
 ;; 计算给定值的反正弦值。
 ;;
@@ -40,25 +36,34 @@
 ;; 当参数不是实数或超出范围时抛出错误。
 ;; wrong-number-of-args
 ;; 当参数数量不为1时抛出错误。
-
 ;; asin 基本测试
 (check (asin 0) => 0)
 (check (asin 1) => 1.5707963267948966)
 (check (asin -1) => -1.5707963267948966)
-
 ;; 特殊值测试
-(check (asin (/ (sqrt 2) 2)) => 0.7853981633974484)
-(check (asin (/ (sqrt 3) 2)) => 1.0471975511965976)
-
+(check (asin (/ (sqrt 2) 2))
+  =>
+  0.7853981633974484
+) ;check
+(check (asin (/ (sqrt 3) 2))
+  =>
+  1.0471975511965976
+) ;check
 ;; 边界测试
-(check (asin 0.000001) => 1.0000000000001666e-6)
-
+(check (asin 0.000001)
+  =>
+  1.0000000000001666e-6
+) ;check
 ;; 有理数测试
 (check (asin 2/3) => 0.7297276562269663)
-
 ;; 错误处理测试
-(check-catch 'wrong-type-arg (asin "hello"))
-(check-catch 'wrong-number-of-args (asin))
-(check-catch 'wrong-number-of-args (asin 1 2))
-
+(check-catch 'wrong-type-arg
+  (asin "hello")
+) ;check-catch
+(check-catch 'wrong-number-of-args
+  (asin)
+) ;check-catch
+(check-catch 'wrong-number-of-args
+  (asin 1 2)
+) ;check-catch
 (check-report)
