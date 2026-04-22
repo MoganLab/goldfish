6c479601a082ae0bcb6284e77920fc2902c41350
Da Shen <da@liii.pro>
2026-04-22 23:48:48 +0800

    [200_74] Remove GMP block: tanh_p_p big number cases (31 lines)

--- src/s7.c
+++ src/s7.c
@@ -16076,38 +16076,6 @@ static s7_pointer tanh_p_p(s7_scheme *sc, s7_pointer x)
       out_of_range_error_nr(sc, sc->tanh_symbol, int_one, x, no_complex_numbers_string);
 #endif
 
-#if WITH_GMP
-    case T_BIG_INTEGER:
-      mpfr_set_z(sc->mpfr_1, big_integer(x), MPFR_RNDN);
-      goto BIG_REAL_TANH;
-    case T_BIG_RATIO:
-      mpfr_set_q(sc->mpfr_1, big_ratio(x), MPFR_RNDN);
-      goto BIG_REAL_TANH;
-    case T_BIG_REAL:
-      if (mpfr_nan_p(big_real(x))) return(x);
-      mpfr_set(sc->mpfr_1, big_real(x), MPFR_RNDN);
-    BIG_REAL_TANH:
-      if (mpfr_cmp_d(sc->mpfr_1, TANH_LIMIT) > 0) return(real_one);
-      if (mpfr_cmp_d(sc->mpfr_1, -TANH_LIMIT) < 0) return(make_real(sc, -1.0));
-      mpfr_tanh(sc->mpfr_1, sc->mpfr_1, MPFR_RNDN);
-      return(mpfr_to_big_real(sc, sc->mpfr_1));
-    case T_BIG_COMPLEX:
-      if ((MPC_INEX_RE(mpc_cmp_si_si(big_complex(x), TANH_LIMIT, 1))) > 0)
-	return(real_one);
-      if ((MPC_INEX_RE(mpc_cmp_si_si(big_complex(x), -TANH_LIMIT, 1))) < 0)
-	return(make_real(sc, -1.0));
-      if ((mpfr_nan_p(mpc_imagref(big_complex(x)))) ||
-	  (mpfr_inf_p(mpc_imagref(big_complex(x)))))
-	{
-	  if (mpfr_cmp_ui(mpc_realref(big_complex(x)), 0) == 0)
-	    return(make_complex_not_0i(sc, 0.0, NAN)); /* match non-bignum choice */
-	  return(complex_NaN);
-	}
-      mpc_tanh(sc->mpc_1, big_complex(x), MPC_RNDNN);
-      if (mpfr_zero_p(mpc_imagref(sc->mpc_1)))
-	return(mpfr_to_big_real(sc, mpc_realref(sc->mpc_1)));
-      return(mpc_to_number(sc, sc->mpc_1));
-#endif
     default:
       return(method_or_bust_p(sc, x, sc->tanh_symbol, a_number_string));
     }
