584d42913df1334bea583af6785f11c78be1bd27
Da Shen <da@liii.pro>
2026-04-22 23:45:52 +0800

    [200_74] Remove GMP blocks: log_chooser, sinh_p_p real and complex limits, sinh big number cases

--- src/s7.c
+++ src/s7.c
@@ -15974,14 +15974,12 @@ static s7_pointer g_int_log2(s7_scheme *sc, s7_pointer args)
 
 static s7_pointer log_chooser(s7_scheme *sc, s7_pointer func, int32_t args, s7_pointer expr)
 {
-#if !WITH_GMP
   if (args == 2)
     {
       s7_pointer x = cadr(expr), y = caddr(expr);
       if ((is_t_integer(y)) && (integer(y) == 2) && (is_t_integer(x)) && (integer(x) > 0))
 	return(sc->int_log2);
     }
-#endif
   return(func);
 }
 
@@ -15996,49 +15994,15 @@ static s7_pointer sinh_p_p(s7_scheme *sc, s7_pointer x)
     case T_REAL:
       {
 	s7_double y = s7_real(x);
-#if WITH_GMP
-	if (fabs(y) > SINH_LIMIT)
-	  {
-	    mpfr_set_d(sc->mpfr_1, y, MPFR_RNDN);
-	    mpfr_sinh(sc->mpfr_1, sc->mpfr_1, MPFR_RNDN);
-	    return(mpfr_to_big_real(sc, sc->mpfr_1));
-	  }
-#endif
 	return(make_real(sc, sinh(y)));
       }
     case T_COMPLEX:
-#if WITH_GMP
-      if ((fabs(real_part(x)) > SINH_LIMIT) || (fabs(imag_part(x)) > SINH_LIMIT))
-	{
-	  mpc_set_d_d(sc->mpc_1, real_part(x), imag_part(x), MPC_RNDNN);
-	  mpc_sinh(sc->mpc_1, sc->mpc_1, MPC_RNDNN);
-	  return(mpc_to_number(sc, sc->mpc_1));
-	}
-#endif
 #if HAVE_COMPLEX_NUMBERS
       return(c_complex_to_s7(sc, csinh(to_c_complex(x))));
 #else
       out_of_range_error_nr(sc, sc->sinh_symbol, int_one, x, no_complex_numbers_string);
 #endif
 
-#if WITH_GMP
-    case T_BIG_INTEGER:
-      mpfr_set_z(sc->mpfr_1, big_integer(x), MPFR_RNDN);
-      mpfr_sinh(sc->mpfr_1, sc->mpfr_1, MPFR_RNDN);
-      return(mpfr_to_big_real(sc, sc->mpfr_1));
-    case T_BIG_RATIO:
-      mpfr_set_q(sc->mpfr_1, big_ratio(x), MPFR_RNDN);
-      mpfr_sinh(sc->mpfr_1, sc->mpfr_1, MPFR_RNDN);
-      return(mpfr_to_big_real(sc, sc->mpfr_1));
-    case T_BIG_REAL:
-      mpfr_sinh(sc->mpfr_1, big_real(x), MPFR_RNDN);
-      return(mpfr_to_big_real(sc, sc->mpfr_1));
-    case T_BIG_COMPLEX:
-      mpc_sinh(sc->mpc_1, big_complex(x), MPC_RNDNN);
-      if (mpfr_zero_p(mpc_imagref(sc->mpc_1)))
-	return(mpfr_to_big_real(sc, mpc_realref(sc->mpc_1)));
-      return(mpc_to_number(sc, sc->mpc_1));
-#endif
     default:
       return(method_or_bust_p(sc, x, sc->sinh_symbol, a_number_string));
     }
