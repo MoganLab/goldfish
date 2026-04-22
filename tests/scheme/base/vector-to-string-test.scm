2a5dc429e7e48c5760b81259e7df4a359934e4db
Da Shen <da@liii.pro>
2026-04-22 14:25:48 +0800

    [216_7] Add tests for vector->string and string->vector

---
+++ tests/scheme/base/vector-to-string-test.scm
@@ -0,0 +1,50 @@
+(import (liii check))
+(import (scheme base))
+(check-set-mode! 'report-failed)
+;; vector->string
+;; 将字符向量转换为字符串。
+;;
+;; 语法
+;; ----
+;; (vector->string vector)
+;;
+;; 参数
+;; ----
+;; vector : vector?
+;; 由字符组成的向量。
+;;
+;; 返回值
+;; ------
+;; string?
+;; 由向量中字符按顺序组成的新字符串。
+;;
+;; 说明
+;; ----
+;; 1. 空向量返回空字符串
+;; 2. 向量中每个元素必须是字符
+(check (vector->string #()) => "")
+(check (vector->string #(#\a)) => "a")
+(check (vector->string #(#\a #\b #\c))
+  =>
+  "abc"
+) ;check
+(check (vector->string #(#\1 #\2))
+  =>
+  "12"
+) ;check
+(check (string-length (vector->string #(#\x #\y #\z))
+       ) ;string-length
+  =>
+  3
+) ;check
+(check-catch 'wrong-number-of-args
+  (vector->string)
+) ;check-catch
+(check-catch 'wrong-type-arg
+  (vector->string '())
+) ;check-catch
+(check-catch 'wrong-type-arg
+  (vector->string #(1 2))
+) ;check-catch
+
+(check-report)
