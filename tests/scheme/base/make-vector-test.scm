85049020416d21ac76fedacc43d30636f551c9f8
Da Shen <da@liii.pro>
2026-04-22 14:19:59 +0800

    [216_7] Add tests for vector? make-vector and vector

---
+++ tests/scheme/base/make-vector-test.scm
@@ -0,0 +1,51 @@
+(import (liii check))
+(import (scheme base))
+(check-set-mode! 'report-failed)
+;; make-vector
+;; 创建指定长度的新向量，可选填充值。
+;;
+;; 语法
+;; ----
+;; (make-vector k [fill])
+;;
+;; 参数
+;; ----
+;; k : exact-nonnegative-integer?
+;; 向量的长度，必须是非负整数。
+;; fill : any
+;; 可选，用于填充向量的值，默认为未指定。
+;;
+;; 返回值
+;; ------
+;; vector?
+;; 长度为 k 的新向量。
+;;
+;; 说明
+;; ----
+;; 1. k 必须是非负整数
+;; 2. 不提供 fill 时，各元素的初始值由实现决定
+(check (make-vector 0) => #())
+(check (vector-length (make-vector 3))
+  =>
+  3
+) ;check
+(check (make-vector 3 #\a)
+  =>
+  #(#\a #\a #\a)
+) ;check
+(check (make-vector 2 'x) => #(x x))
+(check (make-vector 1 42) => #(42))
+(check-catch 'wrong-number-of-args
+  (make-vector)
+) ;check-catch
+(check-catch 'wrong-type-arg
+  (make-vector -1)
+) ;check-catch
+(check-catch 'wrong-type-arg
+  (make-vector 'a)
+) ;check-catch
+(check-catch 'wrong-type-arg
+  (make-vector 1.5)
+) ;check-catch
+
+(check-report)
---
+++ tests/scheme/base/vector-p-test.scm
@@ -0,0 +1,38 @@
+(import (liii check))
+(import (scheme base))
+(check-set-mode! 'report-failed)
+;; vector?
+;; 判断对象是否为向量。
+;;
+;; 语法
+;; ----
+;; (vector? obj)
+;;
+;; 参数
+;; ----
+;; obj : any
+;; 任意对象。
+;;
+;; 返回值
+;; ------
+;; boolean?
+;; 如果对象是向量则返回 #t，否则返回 #f。
+;;
+;; 说明
+;; ----
+;; 1. 空向量也是向量
+;; 2. 列表不是向量
+(check (vector? #(1 2 3)) => #t)
+(check (vector? #()) => #t)
+(check (vector? '(1 2 3)) => #f)
+(check (vector? "abc") => #f)
+(check (vector? 123) => #f)
+(check (vector? 'vector) => #f)
+(check-catch 'wrong-number-of-args
+  (vector?)
+) ;check-catch
+(check-catch 'wrong-number-of-args
+  (vector? 1 2)
+) ;check-catch
+
+(check-report)
