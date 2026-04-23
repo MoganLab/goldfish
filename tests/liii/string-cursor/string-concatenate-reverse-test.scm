1dbdd2dafe411945e31aaeb6880ef95576bd263a
Darcy Shen <da@liii.pro>
2026-04-23 22:48:43 +0800

    [200_75] 添加 string-concatenate-reverse 单元测试和文档

---
+++ tests/liii/string-cursor/string-concatenate-reverse-test.scm
@@ -0,0 +1,38 @@
+(import (liii check))
+(import (liii string-cursor))
+
+(check-set-mode! 'report-failed)
+
+;; string-concatenate-reverse
+;; 将字符串列表按逆序连接成一个字符串。
+;;
+;; 语法
+;; ----
+;; (string-concatenate-reverse string-list [final-string end])
+;;
+;; 参数
+;; ----
+;; string-list : list of string?
+;; 要连接的字符串列表
+;;
+;; final-string : string (可选)
+;; 最后附加的字符串，默认为空字符串
+;;
+;; end : integer (可选)
+;; final-string 的结束位置（字符索引），默认为 final-string 长度
+;;
+;; 返回值
+;; ------
+;; string?
+;; 逆序连接后的新字符串
+;;
+;; 说明
+;; ----
+;; 1. 先反转列表顺序，再连接
+;; 2. 支持可选的 final-string 和 end 参数
+
+(check (string-concatenate-reverse '("a" "b" "c")) => "cba")
+(check (string-concatenate-reverse '()) => "")
+(check (string-concatenate-reverse '("a" "b" "c") "xyz" 2) => "cbaxy")
+
+(check-report)
