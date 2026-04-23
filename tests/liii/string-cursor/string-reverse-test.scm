1df7774cb5dd24fc3e18bdfd8b0a06cba3feccb0
Darcy Shen <da@liii.pro>
2026-04-23 22:47:47 +0800

    [200_75] 添加 string-reverse 单元测试和文档

---
+++ tests/liii/string-cursor/string-reverse-test.scm
@@ -0,0 +1,44 @@
+(import (liii check))
+(import (liii string-cursor))
+
+(check-set-mode! 'report-failed)
+
+;; string-reverse
+;; 反转字符串中的字符顺序。
+;;
+;; 语法
+;; ----
+;; (string-reverse s [start end])
+;;
+;; 参数
+;; ----
+;; s : string
+;; 要反转的字符串
+;;
+;; start : integer (可选)
+;; 反转起始位置（字符索引），默认为0
+;;
+;; end : integer (可选)
+;; 反转结束位置（字符索引），默认为字符串字符数
+;;
+;; 返回值
+;; ------
+;; string?
+;; 反转后的新字符串
+;;
+;; 说明
+;; ----
+;; 1. 返回新字符串，不修改原字符串
+;; 2. 支持 Unicode 字符，包括中文和 emoji
+;; 3. 支持可选的 start/end 参数指定子串反转
+
+;; 基本测试
+(check (string-reverse "abc") => "cba")
+(check (string-reverse "") => "")
+(check (string-reverse "abcdef" 1 4) => "dcb")
+
+;; Unicode 测试
+(check (string-reverse "中文") => "文中")
+(check (string-reverse "🎉🎊") => "🎊🎉")
+
+(check-report)
