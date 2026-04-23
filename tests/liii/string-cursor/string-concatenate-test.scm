97f26c9642a19dc0c3947fde7847117b248e0257
Darcy Shen <da@liii.pro>
2026-04-23 22:48:12 +0800

    [200_75] 添加 string-concatenate 单元测试和文档

---
+++ tests/liii/string-cursor/string-concatenate-test.scm
@@ -0,0 +1,32 @@
+(import (liii check))
+(import (liii string-cursor))
+
+(check-set-mode! 'report-failed)
+
+;; string-concatenate
+;; 将字符串列表连接成一个字符串。
+;;
+;; 语法
+;; ----
+;; (string-concatenate string-list)
+;;
+;; 参数
+;; ----
+;; string-list : list of string?
+;; 要连接的字符串列表
+;;
+;; 返回值
+;; ------
+;; string?
+;; 连接后的新字符串
+;;
+;; 说明
+;; ----
+;; 1. 列表为空时返回空字符串
+;; 2. 支持 Unicode 字符串
+
+(check (string-concatenate '("a" "b" "c")) => "abc")
+(check (string-concatenate '()) => "")
+(check (string-concatenate '("中文" "测试")) => "中文测试")
+
+(check-report)
