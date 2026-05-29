fcc0278954f679ae6233138262e793b4bf2c2a6b
Da Shen <da@liii.pro>
2026-05-29 12:39:40 +0800

    [0064] 将 (liii njson) 的 C++ 实现从 goldfish.hpp 迁移到 src/liii_njson.cpp

--- xmake.lua
+++ xmake.lua
@@ -104,6 +104,7 @@ target ("goldfish") do
     end
     add_files ("src/goldfish.cpp")
     add_files ("src/liii_subprocess.cpp")
+    add_files ("src/liii_njson.cpp")
     add_files ("src/s7.c", {languages = "c11"})
     add_files ("src/s7_scheme_complex.c", {languages = "c11"})
     add_files ("src/s7_scheme_char.c", {languages = "c11"})
