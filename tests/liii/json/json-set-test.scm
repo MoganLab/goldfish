fedc17f45ef25f2a850144c8325c808b906eede5
Darcy Shen <da@liii.pro>
2026-09-08 14:02:44 +0800

    [0154] 修复 json-drop 回调期造环死循环及 json-set/reduce 列表截短脏数据问题 (#986)

--- tests/liii/json/json-set-test.scm
+++ tests/liii/json/json-set-test.scm
@@ -194,12 +194,20 @@
 
 ;; 迭代期间列表被谓词修改变长时的越界保护（防止非法写 nil 导致 Crash）
 (let* ((j (list (cons 'a 1) (cons 'b 2)))
-       (res (json-set j (lambda (k)
-                          (when (eq? k 'a)
-                            (set-cdr! (cdr j) (list (cons 'c 3))))
-                          #f)
-                      0)))
+       (res (json-set j
+              (lambda (k) (when (eq? k 'a) (set-cdr! (cdr j) (list (cons 'c 3)))) #f)
+              0
+            ) ;json-set
+       ) ;res
+      ) ;
   (check-true (list? res))
 ) ;let*
+
+;; 迭代期间列表被谓词修改改短时抛出 value-error（防止残留未填充骨架）
+(check-catch 'value-error
+  (let ((j (list (cons 'a 1) (cons 'b 2))))
+    (json-set j (lambda (k) (when (eq? k 'a) (set-cdr! j '())) #f) 0)
+  ) ;let
+) ;check-catch
 
 (check-report)
