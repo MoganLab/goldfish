3b8a7f843e3d226d487d03c513c303c73a54bba4
Da Shen <da@liii.pro>
2026-09-08 11:53:29 +0800

    [0154] 修复 reverse-vector->list 参数边界校验缺失问题

--- goldfish/srfi/srfi-133.scm
+++ goldfish/srfi/srfi-133.scm
@@ -405,6 +405,29 @@
     ) ;define
 
     (define* (reverse-vector->list vec (start 0) (end (vector-length vec)))
+      (unless (vector? vec)
+        (error 'wrong-type-arg
+          "reverse-vector->list: first argument must be a vector"
+          vec
+        ) ;error
+      ) ;unless
+      (unless (integer? start)
+        (error 'wrong-type-arg "reverse-vector->list: start must be an integer" start)
+      ) ;unless
+      (unless (integer? end)
+        (error 'wrong-type-arg "reverse-vector->list: end must be an integer" end)
+      ) ;unless
+      (let ((len (vector-length vec)))
+        (when (or (< start 0) (> start len))
+          (error 'out-of-range "reverse-vector->list: start out of range" start)
+        ) ;when
+        (when (or (< end 0) (> end len))
+          (error 'out-of-range "reverse-vector->list: end out of range" end)
+        ) ;when
+        (when (> start end)
+          (error 'out-of-range "reverse-vector->list: start > end" start end)
+        ) ;when
+      ) ;let
       (let loop
         ((i start) (acc '()))
         (if (= i end) acc (loop (+ i 1) (cons (vector-ref vec i) acc)))
