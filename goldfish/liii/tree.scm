e696633bc2f4cb97252509c7b621fd3a420f58c9
Darcy Shen <da@liii.pro>
2026-09-14 23:34:15 +0800

    [0179] 在 (liii tree) 库中新增 tree-depth 函数 (#1023)

--- goldfish/liii/tree.scm
+++ goldfish/liii/tree.scm
@@ -1,6 +1,28 @@
 (define-library (liii tree)
   (export tree-cyclic? tree-leaves tree-memq? tree-member? tree-set-memq
-    tree-count
+    tree-count tree-depth
   ) ;export
-  (import (scheme base))
+  (import (scheme base) (liii error))
+  (begin
+    (define (tree-depth tree)
+      (if (tree-cyclic? tree)
+        (value-error "tree-depth: tree is cyclic: ~S" tree)
+        (let loop
+          ((x tree))
+          (if (not (pair? x))
+            0
+            (+ 1
+              (let elt-loop
+                ((rest x) (max-d 0))
+                (if (not (pair? rest))
+                  max-d
+                  (elt-loop (cdr rest) (max max-d (loop (car rest))))
+                ) ;if
+              ) ;let
+            ) ;+
+          ) ;if
+        ) ;let
+      ) ;if
+    ) ;define
+  ) ;begin
 ) ;define-library
