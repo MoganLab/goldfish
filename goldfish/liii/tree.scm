f2382c78f752e464178cd20a90ced6662885ff4c
Darcy Shen <da@liii.pro>
2026-09-15 00:12:13 +0800

    [0179] 优化 tree-depth 透明穿透 quote 语法糖形式 (#1024)

--- goldfish/liii/tree.scm
+++ goldfish/liii/tree.scm
@@ -4,23 +4,32 @@
   ) ;export
   (import (scheme base) (liii error))
   (begin
+    (define (quote-form? x)
+      (and (pair? x)
+        (or (eq? (car x) 'quote) (eq? (car x) #_quote))
+        (pair? (cdr x))
+        (null? (cddr x))
+      ) ;and
+    ) ;define
+
     (define (tree-depth tree)
       (if (tree-cyclic? tree)
         (value-error "tree-depth: tree is cyclic: ~S" tree)
         (let loop
           ((x tree))
-          (if (not (pair? x))
-            0
-            (+ 1
-              (let elt-loop
-                ((rest x) (max-d 0))
-                (if (not (pair? rest))
-                  max-d
-                  (elt-loop (cdr rest) (max max-d (loop (car rest))))
-                ) ;if
-              ) ;let
-            ) ;+
-          ) ;if
+          (cond ((not (pair? x)) 0)
+                ((quote-form? x) (loop (cadr x)))
+                (else (+ 1
+                        (let elt-loop
+                          ((rest x) (max-d 0))
+                          (if (not (pair? rest))
+                            max-d
+                            (elt-loop (cdr rest) (max max-d (loop (car rest))))
+                          ) ;if
+                        ) ;let
+                      ) ;+
+                ) ;else
+          ) ;cond
         ) ;let
       ) ;if
     ) ;define
