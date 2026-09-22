0d4d7f2be2081e4859043a3b2d4a44ed0a426927
Darcy Shen <da@liii.pro>
2026-09-22 13:48:11 +0800

    [1000] pyfmt 支持整数前导零补齐与修饰符解析

--- goldfish/liii/string.scm
+++ goldfish/liii/string.scm
@@ -10,6 +10,7 @@
   ) ;export
   (import (except (srfi srfi-13) string-replace)
     (scheme base)
+    (scheme char)
     (liii base)
     (liii error)
     (liii unicode)
@@ -83,6 +84,64 @@
         (assoc key alist equal?)
       ) ;define
 
+      ;; ; 解析 ) 之后的占位符说明符 [flags][width][.precision]type
+      ;; ; 成功时返回 (placeholder-end type-char flags width precision)，失败时返回 #f
+      (define (parse-spec str start)
+        (let ((len (string-length str)))
+          (let loop
+            ((i start) (flags "") (width "") (precision "") (stage 'flags))
+            (if (>= i len)
+              #f
+              (let ((c (string-ref str i)))
+                (cond
+                 ((and (eq? stage 'flags) (string-contains "-+ #0" (string c)))
+                  (loop (+ i 1) (string-append flags (string c)) width precision 'flags)
+                 ) ;
+                 ((char-numeric? c)
+                  (if (eq? stage 'precision)
+                    (loop (+ i 1) flags width (string-append precision (string c)) 'precision)
+                    (loop (+ i 1) flags (string-append width (string c)) precision 'width)
+                  ) ;if
+                 ) ;
+                 ((and (char=? c #\.) (memq stage '(flags width)))
+                  (loop (+ i 1) flags width precision 'precision)
+                 ) ;
+                 ((char-alphabetic? c)
+                  (list (+ i 1)
+                    c
+                    flags
+                    (if (string-null? width) #f (string->number width))
+                    (if (string-null? precision) #f (string->number precision))
+                  ) ;list
+                 ) ;
+                 (else #f)
+                ) ;cond
+              ) ;let
+            ) ;if
+          ) ;let
+        ) ;let
+      ) ;define
+
+      ;; ; 按 flags 与 width 补齐整数字符串：0 前导零，- 左对齐，默认右对齐空格补齐
+      (define (pad-int val-str flags width)
+        (if (not width)
+          val-str
+          (let ((pad (- width (string-length val-str))))
+            (cond ((<= pad 0) val-str)
+                  ((string-contains flags "-") (string-append val-str (make-string pad #\space)))
+                  ((string-contains flags "0")
+                   ;; 负号不参与补零，保持符号在最前面
+                   (if (string-prefix? "-" val-str)
+                     (string-append "-" (make-string pad #\0) (substring val-str 1))
+                     (string-append (make-string pad #\0) val-str)
+                   ) ;if
+                  ) ;
+                  (else (string-append (make-string pad #\space) val-str))
+            ) ;cond
+          ) ;let
+        ) ;if
+      ) ;define
+
       (let ((salist (plist->salist plist)) (len (string-length format-string)))
         (let loop
           ((i 0) (parts '()))
@@ -93,18 +152,16 @@
                 (let ((end-pos (string-position ")" format-string (+ pos 2))))
                   (if (and end-pos (> end-pos (+ pos 2)))
                     (let* ((key (substring format-string (+ pos 2) end-pos))
-                           (type-pos (+ end-pos 1))
-                           (has-type? (< type-pos len))
-                           (type-char (if has-type? (string-ref format-string type-pos) #\s))
-                           (placeholder-end (if has-type? (+ type-pos 1) (+ end-pos 1)))
+                           (spec (parse-spec format-string (+ end-pos 1)))
+                           (placeholder-end (if spec (car spec) (+ end-pos 1)))
                            (placeholder (substring format-string pos placeholder-end))
                            (pair (lookup-pair key salist))
                            (val (and pair (cdr pair)))
                            (val-str
                              (cond ((not pair) placeholder)
-                                   ((char=? type-char #\d)
+                                   ((and spec (memv (cadr spec) (list #\d #\i)))
                                     (if (number? val)
-                                      (number->string val)
+                                      (pad-int (number->string val) (caddr spec) (cadddr spec))
                                       (type-error "pyfmt: %(key)d requires number")
                                     ) ;if
                                    ) ;
